7443ecd498cf9a9cd7b80171c209574d177645e2
udayasimha97 <139774407+udayasimha97@users.noreply.github.com>
2025-06-13 15:21:41 +0530

    Fixed: JSX issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ImageGallery from "./components/imageGallery/ImageGallery.js"
+import ImageGallery from "./components/imageGallery/ImageGallery.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
